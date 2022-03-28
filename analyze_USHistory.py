45dcec6b76d7a4cc3bb2260597dafa22b1848b7b
kzhong1 <kzhong1@babson.edu>
2022-03-28 00:46:13 -0400

    assignment2

---
+++ analyze_USHistory.py
@@ -0,0 +1,54 @@
+from unittest import skip
+
+def skip_header(line):
+
+    return line.split('=--', 1)[1]
+       
+# def process_line(line, keyword, sentence):
+#     words = line.split( ) 
+#     for word in words: 
+#         if word.endswith('.'):
+#             sentence += f' {word}'
+#             if sentence.__contains__(keyword):
+#                 print(sentence)
+#                 print('\n')
+#                 sentence = ''
+#             else: 
+#                 sentence += f' {word}'
+
+def find_keyword_sentence(keyword):
+    f = open('historyofUS.txt')
+
+    sentence = ''
+
+    for line in f: 
+        if line.startswith('='): 
+           first_line = skip_header(line)
+           words = first_line.split( )
+           for word in words:
+                if word.endswith('.'):
+                    sentence += f' {word}'
+                    if sentence.__contains__(keyword):
+                        print(sentence)
+                        print('\n')
+                    sentence = ''
+                else: 
+                    sentence += f' {word}'
+        else: 
+            words = line.split()
+            for word in words:
+                if word.endswith('.'):
+                    sentence += f' {word}'
+                    if sentence.__contains__(keyword):
+                       # sentence.replace(keyword, f'* {keyword}')
+                        print(sentence)
+                        print('\n')
+                    sentence = ''
+                else: 
+                    sentence += f' {word}'
+
+if __name__ == '__main__':
+    find_keyword_sentence('war')
+
+
+
